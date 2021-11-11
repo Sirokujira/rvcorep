815466a3bf4b4c353a9f9ed24029740c20c49e76
Ubuntu User <toru753@gmail.com>
2021-11-11 17:58:28 +0900

    modiied tcl files

--- vivado.tcl
+++ vivado.tcl
@@ -59,7 +59,10 @@ set_param general.maxThreads $vivado_num_threads
 #synthesize_ip "dram" [list clk_wiz_0 mig_7series_0]
 #synthesize_ip "." [list clk_wiz_1]
 #synthesize_ip "main.srcs/sources_1/ip" [list clk_wiz_0]
+create_bd_design main
 set clk_wiz_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz_0 ]
+apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" } [get_bd_cells zynq_ultra_ps_e_0]
+set zynq_ultra_ps_e_0 [get_bd_cells zynq_ultra_ps_e_0]
 
 # verilog files
 read_verilog {
@@ -116,7 +119,6 @@ set impl_start_time [clock clicks -milliseconds]
 # additional constraint files (for implementation)
 #read_xdc constraints_timing.xdc
 # used in implementation
-#create_generated_clock -name user_design_clk [get_pins CLK]
 create_generated_clock -name user_design_clk [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
 set_clock_groups -asynchronous -group {user_design_clk}
 
@@ -152,6 +154,9 @@ if {$timing_met} {
 set impl_finish_time [clock clicks -milliseconds]
 set impl_time [expr ($impl_finish_time - $impl_start_time) / 1000]
 puts "Implementation time = [expr $impl_time / 60]:[format %02d [expr $impl_time % 60]]"
+
+#
+save_bd_design
 
 #########################################################################
 # generate bitstream file
